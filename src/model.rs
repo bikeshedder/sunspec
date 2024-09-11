cb401adf4239e46f82da4a7f02f3d4f18ee9c2a6
Michael P. Jung <michael.jung@terreon.de>
2024-09-11 12:58:40 +0200

    WiP groups

--- src/model.rs
+++ src/model.rs
@@ -7,6 +7,8 @@ use crate::{DecodeError, Models};
 pub trait Model: Sized {
     /// Model ID
     const ID: u16;
+    /// Model length
+    const LEN: u16;
     /// Parse model points from a given u16 slice
     fn from_data(data: &[u16]) -> Result<Self, DecodeError>;
     /// Get model address from discovered models struct
